e9077076124543fed697362a6cef7bfe4fcc4807
mlisocialure <aml1648@gmail.com>
2016-08-15 11:23:52 -0700

    mealcalculator

---
+++ unit1/meal cal github.js
@@ -0,0 +1,37 @@
+var diners=[
+{'name':'adam', 'entree1':'ramen', 'price1':11.5, 'entree2': 'miso', 'price2' : 3},
+
+{'name':'bobby', 'entree1': 'udon', 'price1':10.69, 'entree2': 'gyoza', 'price2' : 4.5},
+
+{'name':'charly', 'entree1': 'teriyaki chicken', 'price':12, 'entree2': 'miso', 'price2' : 3},
+];
+
+var entrees1_total=0;
+for (var d in diners){
+     entrees1_total +=  diners[d].price1; //total expense of entree1 for all diners
+
+     diners[d].tax1 = diners[d].price1* 0.082; // method for sales tax
+
+     entrees1_total += diners[d].tax1; //total entree1 price including sales tax
+     }
+
+var entrees2_total=0;
+for (var d in diners){
+    entrees2_total += diners[d].price2;
+
+    diners[d].tax2 = diners[d]* price2 * 0.082;
+
+    entrees2_total += diners[d].tax2;
+    }
+
+var total = entree1_total + entree2_total;
+
+var total_bill = total*1.2;   //tips
+var tips = total*.2;
+
+console.log("total is: " + total_bill.toString());
+console.log("total tips for the waitress is: " + tips.toString());
+
+for (var d in diners) {
+    console.log(diners[d].name + " spends " + (diners[d].price1 + diners[d].tax1)+(diners[d].price2 + diners[d].tax2));
+        }   // print out total spent for each patron
