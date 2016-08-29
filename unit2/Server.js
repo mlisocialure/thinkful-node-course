160fce22d8cc1a4c3ebc9dd4f1b951bc792bcb1a
mlisocialure <aml1648@gmail.com>
2016-08-29 14:12:48 -0700

    server.js working ast it stands now with get and put

--- unit2/Server.js
+++ unit2/Server.js
@@ -23,5 +23,21 @@ app.use(express.static('public'));
 app.get('/items', function(request, response) {
     response.json(storage.items);
 });
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+app.post('/items', jsonParser, function(request, response) {
+    if (!request.body) {
+        return response.sendStatus(400);
+    }
+    var item = storage.add(request.body.name);
+    response.status(201).json(item);
+});
+
+
+
+
+
+
 
 app.listen(process.env.PORT || 8080, process.env.IP);
