ad239be3af0a67cf406b6b56766f8f2acf931cbf
mlisocialure <aml1648@gmail.com>
2016-09-06 02:16:01 +0000

    added some comments to python bot

--- redditbots/hansoloiknow.py
+++ redditbots/hansoloiknow.py
@@ -12,8 +12,8 @@ response_text = "I know"
 processed = []
 while True:
     for c in comment_stream(r, 'all'):
-        if target_text in c.body and c.id not in processed:
+        if target_text in c.body and c.id not in processed: #if find comment not in cache
             c.reply(response_text)
-            processed.append(c.id)
+            processed.append(c.id)   #then push the response 
             time.sleep(20)
 
