7b4b3cad19c3d2e5aaa40e34e7cb1b92e3a32be3
mlisocialure <aml1648@gmail.com>
2016-09-11 21:52:04 +0000

    added put and delete to shopping list

--- unit2/Server.js
+++ unit2/Server.js
@@ -1,43 +1,60 @@
 var express = require('express');
+var jsonParser = require('body-parser').json();
 
-var Storage = function() {
-    this.items = [];
-    this.id = 0;
-};
-
-Storage.prototype.add = function(name) {
-    var item = {name: name, id: this.id};
+var Storage = {             //creating local database
+  add: function(name) {
+    var item = {name: name, id: this.setId};
     this.items.push(item);
-    this.id += 1;
+    this.setId += 1;
     return item;
+  } 
 };
 
-var storage = new Storage();
+var createStorage = function() {
+  var storage = Object.create(Storage);
+  storage.items = [];
+  storage.setId = 1;
+  return storage;
+}
+
+var storage = createStorage();
+
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
-
 var app = express();
 app.use(express.static('public'));
 
-app.get('/items', function(request, response) {
-    response.json(storage.items);
-});
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-
-app.post('/items', jsonParser, function(request, response) {
-    if (!request.body) {
-        return response.sendStatus(400);
-    }
-    var item = storage.add(request.body.name);
-    response.status(201).json(item);
+app.get('/items', function(req, res) {
+  res.json(storage.items);
 });
 
+app.post('/items', jsonParser, function(req, res) {
+  if (!req.body) {
+    return res.sendStatus(400);    
+  }
+  var item = storage.add(req.body.name);      //adding a new item
+  res.status(201).json(item);
+});
 
+app.put('/items/:id', jsonParser, function(req, res) {  
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  var item = storage.put(req.params.id, req.body.name); //updating new item with id
+  res.status(200).json(item);
+});
 
-
-
-
+app.delete('/items/:id', function(req, res) {
+  var item = storage.remove(req.params.id);   //deleting an item along with its corresponding id
+  if (item) {
+    res.status(200).json(item);                   
+  } else {
+    res.status(400).json({"error": "no item found"});
+  }
+});                                               
 
 app.listen(process.env.PORT || 8080, process.env.IP);
+
+exports.app = app;
+exports.storage = storage;
