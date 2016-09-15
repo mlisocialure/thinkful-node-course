9a0c3e4627ce44a1276005db32783300078f41b0
mlisocialure <aml1648@gmail.com>
2016-09-15 00:34:11 +0000

    wiseau bot updated

--- redditbots/theroom/tommywiseaubot.py
+++ redditbots/theroom/tommywiseaubot.py
@@ -6,11 +6,8 @@ r = praw.Reddit("Tommy Wiseau one liner")
 r.login()
 
 
-target_text = "tommy wiseau"
-response_text = "You are Tearing Me Apart {}!"
-
-target_text1 = "greg sostero"
-response_text1 = "Keep your stupid comments in your pocket! {}!"
+target_text = "wiseau"
+response_text = "You Are Tearing Me Apart {}!"
 
  
 processed = []
@@ -22,14 +19,7 @@ while True:
             processed.append(c.id)   #then push the response 
             time.sleep(2)
 
-    for c1 in comment_stream(r, 'all'):   #posting to all subreddits! it is ok if a few subs end up banning me
-        if target_text1 in c1.body.lower() and c1.id not in processed: #if find comment not in cache
-            print('Johnny is my best friend. :@')
-            c1.reply(response_text1.format(c.author)) #use PRAW's author method to invoke the triggering redditor's username
-            processed.append(c1.id)   #then push the response 
-            time.sleep(2)
-
-        
+    
         
 
         
