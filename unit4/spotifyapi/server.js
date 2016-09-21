d02866d865bf70759b7c4b90ac12cf817444234e
mlisocialure <aml1648@gmail.com>
2016-09-21 22:42:25 +0000

    spotifyapi

--- unit4/spotifyapi/server.js
+++ unit4/spotifyapi/server.js
@@ -1,41 +1,100 @@
-var unirest = require('unirest');
-var express = require('express');
+var http = require('http');
+var https = require('https');
 var events = require('events');
+var querystring = require('querystring');
+var static = require('node-static');
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
-    unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('end', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+    var options = {
+        host: 'api.spotify.com',
+        path: '/v1/' + endpoint + '?' + querystring.stringify(args)
+    };
+    var item = '';
+    var searchReq = https.get(options, function(response) {
+        response.on('data', function(chunk) {
+            item += chunk;
+        });
+        response.on('end', function() {
+            item = JSON.parse(item);
+            emitter.emit('end', item);
+        });
+        response.on('error', function() {
+            emitter.emit('error');
+        });
+    });
     return emitter;
 };
 
+var getRelatedArtists = function(res, artist){
+    var relatedUrl = 'artists/' + artist.id + '/related-artists';
+    var relatedReq = getFromApi(relatedUrl, {
+    });     
 
-var app = express();
-app.use(express.static('public'));
-
-app.get('/search/:name', function(req, res) {
-    var searchReq = getFromApi('search', {
-        q: req.params.name,
-        limit: 1,
-        type: 'artist'
+    relatedReq.on('end', function(relatedArtists){
+        artist.related = relatedArtists.artists;
+        getTopTracks(res, artist, relatedArtists.artists);
     });
 
-    searchReq.on('end', function(item) {
-        var artist = item.artists.items[0];
-        res.json(artist);
+    relatedReq.on('error', function() {
+        res.statusCode = 404;
+        res.end();
     });
+};
 
-    searchReq.on('error', function(code) {
-        res.sendStatus(code);
+var getTopTracks = function(res, artist, relatedArtists){
+    var completed = 0;
+    var checkCompleted = function(){
+        if(completed === relatedArtists.length){
+            res.statusCode = 200;
+            res.end(JSON.stringify(artist));
+        }
+    };
+
+    relatedArtists.forEach(function(relatedArtist){
+        var tracksUrl = 'artists/' + relatedArtist.id + '/top-tracks';
+        var tracksReq = getFromApi(tracksUrl, {
+            country: 'US'
+        });
+
+        tracksReq.on('end', function(tracksItem){
+            relatedArtist.tracks = tracksItem.tracks;
+            completed += 1;
+            checkCompleted();
+        });
+
+        tracksReq.on('error', function(){
+            console.log('No top tracks found for' + artist.name);
+            // Should we return any error here or simply not list tracks?
+        });
     });
+};
+
+
+var fileServer = new static.Server('./public');
+var server = http.createServer(function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    if (req.method == "GET" && req.url.indexOf('/search/') === 0) {
+        var name = decodeURI(req.url.split('/')[2]);
+        var searchReq = getFromApi('search', {
+            q: name,
+            limit: 1,
+            type: 'artist'
+        });
+
+        searchReq.on('end', function(item) {
+            var artist = item.artists.items[0];
+            getRelatedArtists(res, artist);
+        });
+
+        searchReq.on('error', function() {
+            res.statusCode = 404;
+            res.end();
+        });
+    }
+    else {
+        fileServer.serve(req, res);
+    }
 });
 
-app.listen(process.env.PORT || 8080);
+server.listen(8080);
